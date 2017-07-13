afcaeebf9bbb7c1d4a3c32632052218f1f50d872
nicolas.acquaviva <nicoacq@gmail.com>
2017-07-13 17:43:11 -0300

    Changed printf for echo

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ TMUX_TPM_DIR=~/.tmux/plugins/tpm/
 VIMRC_FILE=~/.vimrc
 
 if [ -f $TMUX_FILE ]; then
-    printf "tmux.conf already exists. Moving it to .tmux.conf.bak"
+    echo "tmux.conf already exists. Moving it to .tmux.conf.bak"
     rm ~/.tmux.conf.bak
     mv $TMUX_FILE ~/.tmux.conf.bak
 fi
@@ -15,14 +15,14 @@ ln -s ~/dotfiles/.tmux.conf $TMUX_FILE
 printf "Checking for Tmux Package Manager"
 
 if [ -f $TMUX_TPM_DIR/tpm ]; then
-        printf "Tmux Package Manager already installed!"
+        echo "Tmux Package Manager already installed!"
 else
-        printf "Cloning Tmux Package Manager"
+        echo "Cloning Tmux Package Manager"
         git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
 fi
 
 if [ -f $VIMRC_FILE ]; then
-        printf "vimrc file already exists. Moving it to .vimrc.bak"
+        echo "vimrc file already exists. Moving it to .vimrc.bak"
         rm ~/.vimrc.bak
         mv $VIMRC_FILE ~/.vimrc.bak
 fi
