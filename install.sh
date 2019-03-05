df3b2548ac65f035c7b2975cfffdf6ac72a09ce9
Nicolas Acquaviva <nicolas.acquaviva@globant.com>
2019-03-05 19:12:53 -0300

    Install script update (#2)
    * install.sh: code cleanup
    
    * install.sh: code clean up

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+#
+# Style guide: https://google.github.io/styleguide/shell.xml
 
-GIT_CONFIG_FILE=~/.gitconfig
-OHMYSZH_DIR=~/.oh-my-zsh
-TMUX_FILE=~/.tmux.conf
-TMUX_TPM_DIR=~/.tmux/plugins/tpm/
-VIMRC_FILE=~/.vimrc
-ZSH_FILE=~/.zshrc
-VUNDLE=~/.vim/bundle/Vundle.vim
-BASHRC=~/.bashrc
+GIT_CONFIG_FILE=$HOME/.gitconfig
+OHMYSZH_DIR=$HOME/.oh-my-zsh
+TMUX_FILE=$HOME/.tmux.conf
+TMUX_TPM_DIR=$HOME/.tmux/plugins/tpm/
+VIMRC_FILE=$HOME/.vimrc
+ZSH_FILE=$HOME/.zshrc
+VUNDLE=$HOME/.vim/bundle/Vundle.vim
+BASHRC=$HOME/.bashrc
 
 BLUE=$(tput setaf 4)
 GREEN=$(tput setaf 2)
@@ -15,130 +17,151 @@ NORMAL=$(tput sgr0)
 RED=$(tput setaf 1)
 YELLOW=$(tput setaf 3)
 UNDERLINE=$(tput smul)
-BR="\n%s\n"
+BR="\n"
 
-# brew does not like running things as sudo
-if [ ! -x "$(command -v tmux)" ] && [ $OSTYPE == "darwin" ]; then
-        printf "${BR}" "${RED}Tmux not installed... ${NORMAL}Installing it."
-        brew install tmux reattach-to-user-namespace the_silver_searcher
-fi
+# $1 message
+# $2 color
+# $3 underlined
+_echo() {
+  message="\n${2}${1}"
 
-if [ "$EUID" -ne 0 ]; then
-        printf ${BR} "${UNDERLINE}${RED}Please run as root.${NORMAL}"
+  if [[ $3 ]]; then
+    message="$3$message"
+  fi
 
-        exit
-fi
+  echo -e "$message${NORMAL}"
+}
 
-printf "${BR}${UNDERLINE}Installing and configuring dotfiles.${BR}${NORMAL}"
+osx_install() {
+  _echo "MacOS detected" $GREEN $UNDERLINE
 
-if [ ! -x "$(command -v git)" ]; then
-        printf "${BR}" "${RED}git not installed...${NORMAL} Installing it."
-        apt-get install -y git
-fi
+  if [[ ! -x "$(command -v tmux)" ]]; then
+    _echo " > Installing Tmux" $GREEN
+    brew install tmux
+  fi
 
-if [ ! -x "$(command -v curl)" ]; then
-        printf "${BR}" "${RED}curl not installed...${NORMAL} Installing it."
-	apt-get install -y curl
-fi
+  if [[ ! -x "$(command -v zsh)" ]]; then
+    _echo " > Installing Zsh" $GREEN
+    brew install zsh
+  fi
+}
 
-if [ ! -x "$(command -v tmux)" ]; then
-        printf "${BR}" "${RED}tmux not installed...${NORMAL} Installing it."
-	apt-get install -y tmux
-fi
+linux_install() {
+  _echo "Linux detected" $GREEN
 
-if [ ! -x "$(command -v vim)" ]; then
-        printf "${BR}" "${RED}Vim not installed...${NORMAL} What world is this? Installing it."
-        apt-get install -y vim
-fi
+  if [[ ! -x "$(command -v tmux)" ]]; then
+    _echo " > Installing Tmux" $GREEN
+    apt-get install -y tmux
+  fi
 
-if [ ! -x "$(command -v zsh)" ]; then
-        printf ${BR} "${RED}zsh not installed. ${NORMAL}Installing it!"
+  if [[ ! -x "$(command -v curl)" ]]; then
+    _echo " > Installing Curl" $GREEN
+    apt-get install -y curl
+  fi
 
-        if [[ "$OSTYPE" == "linux-gnu" ]]; then
-                apt-get install zsh -Y
-        elif [[ "$OSTYPE" == "darwin" ]]; then
-                brew install zsh
-        fi
-fi
+  if [[ ! -x "$(command -v zsh)" ]]; then
+    _echo " > Installing Zsh" $GREEN
+    apt-get install zsh -Y
+  fi
+}
 
-if [ ! -d $OHMYSZH_DIR ]; then
-        printf ${BR} "${RED}oh my zsh not installed. ${NORMAL}Installing it!"
-        sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-fi
+commons() {
+  if [[ ! -d $OHMYSZH_DIR ]]; then
+    _echo " > Installing Oh My Zsh" $GREEN
+    sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  fi
 
-if [ ! -f $OHMYSZH_DIR/themes/dracula.zsh-theme ]; then
-        TMP_DIR='temporal_directory_i_hope_this_does_not_exist'
+  if [[ -f $ZSH_FILE ]]; then
+    _echo " >> ${ZSH_FILE} already exists. Moving it to ${ZSH_FILE}.bak" $YELLOW
+    if [[ -f $ZSH_FILE.bak ]]; then
+      rm $HOME/.zshrc.bak
+    fi
+    mv $ZSH_FILE $HOME/.zshrc.bak
+  fi
 
-	if [ -d "$TMP_DIR" ]; then
-		rm -rf $TMP_DIR
-	fi
+  ln -s $HOME/dotfiles/.zshrc $ZSH_FILE
 
-        printf ${BR} "${GREEN}Installing dracula zsh theme.${NORMAL}"
-        mkdir ${TMP_DIR}
-        cd ${TMP_DIR}
-        git clone https://github.com/dracula/zsh.git
-        mv zsh/dracula.zsh-theme $OHMYSZH_DIR/themes/
-        rm -rf ${TMP_DIR}
-fi
+  if [[ -f $BASHRC ]]; then
+    _echo " >> ${BASHRC} already exists. Moving it to ${BASHRC}.bak" $YELLOW
+    if [[ -f $BASHRC.bak ]]; then
+      rm $HOME/.bashrc.bak
+    fi
+    mv $BASHRC $HOME/.bashrc.bak
+  fi
 
-if [ -f $ZSH_FILE ]; then
-        printf ${BR} "${YELLOW}${ZSH_FILE} already exists.${NORMAL} Moving it to ${ZSH_FILE}.bak"
-        rm ~/.zshrc.bak
-        mv $ZSH_FILE ~/.zshrc.bak
-fi
+  ln -s $HOME/dotfiles/.bashrc $BASHRC
 
-ln -s ~/dotfiles/.zshrc $ZSH_FILE
+  if [[ -f $GIT_CONFIG_FILE ]]; then
+    _echo " >> ${GIT_CONFIG_FILE} already exists. Moving it to ${GIT_CONFIG_FILE}.bak" $YELLOW
+    if [[ -f $GIT_CONFIG_FILE.bak ]]; then
+      rm $HOME/.gitconfig.bak
+    fi
+    mv $GIT_CONFIG_FILE $HOME/.gitconfig.bak
+  fi
 
-if [ -f $BASHRC ]; then
-        printf ${BR} "${YELLOW}${BASHRC} already exists. ${NORMAL}Moving it to ${BASHRC}.bak"
-        rm ~/.bashrc.bak
-        mv $BASHRC ~/.bashrc.bak
-fi
+  ln -s $HOME/dotfiles/.gitconfig $GIT_CONFIG_FILE
 
-ln -s ~/dotfiles/.bashrc $BASHRC
+  if [[ -f $TMUX_FILE ]]; then
+    _echo " >> ${TMUX_FILE} already exists. Moving it to ${TMUX_FILE}.bak" $YELLOW
+    if [[ -f $TMUX_FILE.bak ]]; then
+      rm $HOME/.tmux.conf.bak
+    fi
+    mv $TMUX_FILE $HOME/.tmux.conf.bak
+  fi
 
-if [ -f $GIT_CONFIG_FILE ]; then
-        printf ${BR} "${YELLOW}${GIT_CONFIG_FILE} already exists. ${NORMAL}Moving it to ${GIT_CONFIG_FILE}.bak"
-        rm ~/.gitconfig.bak
-        mv $GIT_CONFIG_FILE ~/.gitconfig.bak
-fi
+  ln -s $HOME/dotfiles/.tmux.conf $TMUX_FILE
 
-ln -s ~/dotfiles/.gitconfig $GIT_CONFIG_FILE
+  if [[ -f $TMUX_TPM_DIR/tpm ]]; then
+    _echo " >> Updating Tmux plugins" $YELLOW
+    sh $HOME/.tmux/plugins/tpm/bin/update_plugins all
+  else
+    _echo " >> Installing Tmux Package Manager and plugins" $GREEN
+    git clone https://github.com/tmux-plugins/tpm $HOME/.tmux/plugins/tpm/
+    sh $HOME/.tmux/plugins/tpm/scripts/install_plugins.sh
+  fi
 
-if [ -f $TMUX_FILE ]; then
-    printf ${BR} "${YELLOW}${TMUX_FILE} already exists. ${NORMAL}Moving it to ${TMUX_FILE}.bak"
-    rm ~/.tmux.conf.bak
-    mv $TMUX_FILE ~/.tmux.conf.bak
-fi
+  if [[ -f $VIMRC_FILE ]]; then
+    _echo " >> ${VIMRC_FILE} already exists. Moving it to ${VIMRC_FILE}.bak" $YELLOW
+    if [[ -f $VIMRC_FILE.bak ]]; then
+      rm $HOME/.vimrc.bak
+    fi
+    mv $VIMRC_FILE $HOME/.vimrc.bak
+  fi
 
-ln -s ~/dotfiles/.tmux.conf $TMUX_FILE
+  ln -s $HOME/dotfiles/.vimrc $VIMRC_FILE
 
-if [ -f $TMUX_TPM_DIR/tpm ]; then
-        printf ${BR} "${GREEN}Tmux Package Manager already installed!"
-        printf ${BR} "${NORMAL}Updating Tmux plugins"
-        sh ~/.tmux/plugins/tpm/bin/update_plugins all
-else
-        printf ${BR} "${YELLOW}Cloning Tmux Package Manager"
-        git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm/
-        printf ${BR} "${NORMAL}Installing Tmux plugins"
-        sh ~/.tmux/plugins/tpm/scripts/install_plugins.sh
-fi
+  if [[ ! -d $VUNDLE ]]; then
+    _echo " >> Installing Vundle, vim plugin manager." $GREEN
+    git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+  fi
 
-if [ -f $VIMRC_FILE ]; then
-        printf ${BR} "${YELLOW}${VIMRC_FILE} file already exists. ${NORMAL}Moving it to ${VIMRC_FILE}.bak"
-        rm ~/.vimrc.bak
-        mv $VIMRC_FILE ~/.vimrc.bak
-fi
+  # # install vim plugins using Vundle
+  # # -c flag runs command before vim starts up
+  vim -c "PluginInstall" -c "qa!"
 
-if [ ! -d $VUNDLE ]; then
-        printf ${BR} "${YELLOW} Vundle.vim not installed. ${NORMAL}Installing it."
-        git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-fi
+  _echo " > The following symbolic links were created:" $GREEN $UNDERLINE
+  cd $HOME && ls -la | grep "\->" | grep dotfiles | grep -v bak
+}
 
-ln -s ~/dotfiles/.vimrc $VIMRC_FILE
+main() {
+  if [[ "$EUID" -ne 0 ]]; then
+    _echo "Please run as root" $RED $UNDERLINE
+    exit 1
+  fi
 
-# install vim plugins using Vundle
-# -c flag runs command before vim starts up
-vim -c "PluginInstall" -c "qa!"
+  case $OSTYPE in
+    darwin18) osx_install ;;
+    darwin) osx_install ;;
+    linux-gnu) linux_install ;;
+    **)
+      _echo "Unsupported OS ${OSTYPE}" $RED
+      exit 1
+      ;;
+  esac
 
-printf "${BR}${GREEN} DONE! ${BR}${NORMAL}"
+  commons
+
+  _echo "DONE!" $GREEN $UNDERLINE
+}
+
+main
