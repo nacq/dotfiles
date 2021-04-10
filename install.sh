71d2ed3c02a1d63924eefbcefe1abff33bd508ce
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2021-04-10 17:46:30 -0300

    remove unused stuff

--- install.sh
+++ install.sh
@@ -51,33 +51,12 @@ osx_install() {
   commons
 }
 
-# this will install $PACKAGES using `pacman` package manager
-arch_install() {
-  if [[ "$EUID" -ne 0 ]]; then
-    log "Please run as root"
-    exit 1
-  fi
-
-  for package in "${PACKAGES[@]}"; do
-    [[ $package == "silversearcher-ag" ]] && package="the_silver_searcher"
-    [[ -x `which $package` ]] || sudo pacman -S $package && log "$package installed"
-  done
-
-  commons
-
-  exit 0
-}
-
 linux_install() {
   # root check
   # if [[ "$EUID" -ne 0 ]]; then
     # echo " >>> Please run as root"
     # exit 1
   # fi
-
-  OS=`cat /etc/os-release | grep ^ID | awk -F '=' '{ print $2 }'`
-
-  [[ $OS == 'arch' ]] && arch_install
 
   for package in "${PACKAGES[@]}"; do
     # meh
