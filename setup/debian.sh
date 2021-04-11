54997d60fdd89ccee75c57b38ab4db8c4811db6a
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2021-04-11 15:05:55 -0300

    generate config dirs

--- setup/debian.sh
+++ setup/debian.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/bash
 
 REPO_NAME="dotfiles"
+# these directories should exist in this repo
 dirs=(
+  ".config"
   ".gnupg"
 )
 files=(
   ".bashrc"
-  ".gitignore"
+  ".gitconfig"
   ".tmux.conf"
+  ".vimrc"
   ".zshrc"
 )
 packages=(
@@ -45,6 +48,25 @@ check_packages_installed() {
   done
 }
 
+# $1 one of the dirs defined in $dirs in an absolute path format
+generate_nested_configs() {
+  echo "working with $1"
+  [[ -d $1 ]] && ls -a $1 | while read content; do
+    if [[ ! $content == "." && ! $content == ".." ]]; then
+      file_path=$1/$content
+      # remove the "dotfiles/" string
+      destination=${file_path//$REPO_NAME\/''}
+
+      if [[ -d $1/$content ]]; then
+        mkdir -p $destination && generate_nested_configs $1/$content
+      elif [[ -f $1/$content ]]; then
+        # TODO: handle file already exists
+	ln -s $1/$content $destination
+      fi
+    fi
+  done
+}
+
 install_packages() {
   for package in "${packages[@]}"; do
     echo -e "Installing $package..."
@@ -54,7 +76,7 @@ install_packages() {
 
 link_files() {
   for file in "${files[@]}"; do
-    [[ -f $HOME/$file ]] && mv $HOME/$file $HOME/$file$(date +"%Y-%m-%d_%H:%M:%S").bkp 
+    [[ -f $HOME/$file ]] && mv $HOME/$file $HOME/$file_$(date +"%Y-%m-%d_%H:%M:%S").bkp
     ln -sf $HOME/$REPO_NAME/$file $HOME/$file
   done
 }
@@ -63,7 +85,10 @@ echo "Starting Debian based system setup..."
 
 install_packages
 link_files
-# set zsh as the default shell 
+for dir in "${dirs[@]}"; do
+  generate_nested_configs $HOME/$REPO_NAME/$dir
+done
+# set zsh as the default shell
 # NOTE: this requires a logout to take effect
 chsh -s $(which zsh)
 
