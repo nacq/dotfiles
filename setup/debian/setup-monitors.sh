09417df8093433012ddeafd06204c4a68e502733
Nicolas Acquaviva <nicolaseacquaviva@gmail.com>
2021-11-20 12:46:58 -0300

    monitors and logging

--- setup/debian/setup-monitors.sh
+++ setup/debian/setup-monitors.sh
@@ -1,17 +1,10 @@
 #!/bin/sh
-
-xrandr --listproviders &> /dev/null &&
-xrandr --setprovideroutputsource 1 0 &&
-xrandr --setprovideroutputsource 2 0 &&
-xrandr --setprovideroutputsource 3 0 &&
-xrandr --setprovideroutputsource 4 0 &&
-
 #
 #   _ _ _ _ _
 # |           |
 # |           |   _ _ _ _ _ _ _ _ _ _ _
 # |           | |                      |
-# | DVI-I-1-1 | | DVI-I-2-2            |
+# | HDMI-2    | | DP-1                 |
 # |           | |                      |
 # |           | |                      |
 # |           | | _ _ _ _ _ _ _ _ _ _ _|
@@ -20,5 +13,6 @@ xrandr --setprovideroutputsource 4 0 &&
 #                | eDP-1     |
 #                | _ _ _ _ _ |
 #
-xrandr --output DVI-I-2-2 --auto --rotate normal --above eDP-1 &&
-xrandr --output DVI-I-1-1 --auto --rotate right --left-of DVI-I-2-2
+xrandr --output eDP-1 --auto --rotate normal \
+  --output DP-1 --auto --rotate normal --above eDP-1 \
+  --output HDMI-2 --auto --rotate right --left-of DP-1 --scale 1x1
